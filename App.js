fcab0485849852d71a475e6d5301aac3708c2ea0
JohnsonNYC <jkow95@gmail.com>
2020-08-19 16:19:40 -0400

    created header placeholder

--- App.js
+++ App.js
@@ -6,37 +6,6 @@ import { View, SafeAreaView, ImageBackground, Image, Text } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: "column",
-      }}
-    >
-      <ImageBackground
-        source={require("./assets/background.jpg")}
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          resizeMode: "cover",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          source={require("./assets/logo-red.png")}
-          style={{
-            width: 100,
-            height: 100,
-            position: "absolute",
-            top: 100,
-          }}
-        />
-        <Text style={{ position: "absolute", top: 200 }}>
-          Sell What You Need
-        </Text>
-      </ImageBackground>
-      <View style={{ flex: 0.1, backgroundColor: "#fc5c65" }} />
-      <View style={{ flex: 0.1, backgroundColor: "#4ECDC4" }} />
-    </View>
+    <Header/>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,21 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import {View, Text, StyleSheet} from 'react-native'
 
 class Header extends Component {
-  state = {};
-  render() {
-    return <View style={{ flex: 1, backgroundColor: "gold" }}></View>;
-  }
+    state = {  }
+    render() { 
+        return ( 
+            <View style={{flex:0.1, justifyContent:'center', alignItems:'center', backgroundColor:'royalblue'}}>
+              <Text style={style.header}>U n b a k e d</Text>
+            </View>
+        );
+    }
 }
-
+const style = StyleSheet.create({
+  header:{
+    top:10,
+    fontFamily:"Verdana",
+    fontSize:40
+  }
+})
 export default Header;
